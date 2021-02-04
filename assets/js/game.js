32a2dfe20836b13fff188d6c2d8ea766648fb7d7
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-02-03 19:59:47 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -41,7 +41,7 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // use debugger to pause script running and check whats going on at that moment in the code
       // debugger;
@@ -104,14 +104,15 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip the fight. Goodbye!");
         // subtract money from playerMoney for skipping 
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
 
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
     );
@@ -125,8 +126,9 @@ var fight = function (enemyName) {
     }
 
 
-    // remove player's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    // generate random damage value based on enemy attack power
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -141,6 +143,16 @@ var fight = function (enemyName) {
     }
   }
 };
+
+// function to generate a random numeric value
+var randomNumber = function () {
+  var value = Math.floor(Math.random() * 21) + 40;
+
+  return value;
+};
+
+
+
 var shop = function () {
   // ask player what they'd like to do 
   var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store. Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice");
