41e08bf1081b6df8c12354bae0b50dd196c68e2d
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-02-04 23:34:44 -0700

    added high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -48,14 +48,27 @@ var startGame = function () {
 
 
 // function to end the entire game //
-var endGame = function () {
+var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + " . ");
-  } else {
-    window.alert("You've lost your robot in battle.");
+
+  // check local storage for high score, if it is not there use 0
+  var highScore = localStorage.getItem("highscore") || 0
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
   }
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highscore + ". Maybe next time." );
+  }
+
+  // if player is still alive, player wins!
+  // if (playerInfo.health > 0) {
+  //   window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + " . ");
+  // } else {
+  //   window.alert("You've lost your robot in battle.");
+  // }
   //ask the player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
